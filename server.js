4ea8a9caafdd4c90a21c3a680afd4aa047400221
ywkuo227 <ywkuo@outlook.com>
2021-08-23 20:21:30 -0700

    Aug 23rd code update #6: - Introduced Moment and Handlebars-dateFormat libraries. - The app now displays current date in the top right corner. - Introduced codes to lockout Test user to prevent Create/Edit/Delete by Test user. - YWK signing-off.

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -12,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create( {});
 
 const sess = {
   secret: 'Super secret secret',
@@ -31,6 +30,8 @@ app.use(session(sess));
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+hbs.handlebars.registerHelper('dateFormat', require('handlebars-dateformat'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
